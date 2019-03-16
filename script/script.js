baf5857bbf0a3f4070e7278f706af9ad00fcc7fc
Breno Campos Ribeiro <39710607+Brenin1991@users.noreply.github.com>
2019-03-16 20:23:44 -0300

    V 1.0.0

---
+++ script/script.js
@@ -0,0 +1,10 @@
+
+/*
+    2019 - Hard Rock Studio 
+    Simple Material Design by Breno Campos Ribeiro
+
+    --Material Design Text Field--
+
+    GitHub Repository: https://github.com/Brenin1991/simple-material-design
+    License: https://drive.google.com/open?id=11ZTUlB6qpoKk_XgD6_dtKntQOfCsuQj8
+*/
